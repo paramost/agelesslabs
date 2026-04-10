7e6fb7c6ae10ef7ad1526b96f3832bdbbe6dda76
paramost <dwcarey@gmail.com>
2026-04-10 14:49:43 -0500

    Add files via upload

---
+++ eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function(eleventyConfig) {
+
+  // Pass through static assets unchanged
+  eleventyConfig.addPassthroughCopy("src/styles.css");
+  eleventyConfig.addPassthroughCopy("src/favicon.svg");
+
+  // Collections for future content buildout
+  eleventyConfig.addCollection("biomarkers", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/biomarkers/**/*.njk")
+      .sort((a, b) => (a.data.title || "").localeCompare(b.data.title || ""));
+  });
+
+  eleventyConfig.addCollection("blog", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/blog/**/*.njk")
+      .sort((a, b) => b.date - a.date);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+      data: "_data"
+    },
+    templateFormats: ["njk", "html", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
